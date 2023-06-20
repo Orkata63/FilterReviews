1b0f6f6efd300b91b4c7c6c9439823c56fe114b6
Orkata <50708833+Orkata63@users.noreply.github.com>
2023-06-20 11:49:20 +0200

    Update urls.py

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('filter/', include('filter.urls')),
+    path('', include('filter.urls')),
 ]
